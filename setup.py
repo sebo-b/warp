6054c95c47c94c3bcea1529203f0940a45fe1c4e
Sebastian Baberowski <sebastian@baberowski.com>
2021-11-22 00:53:47 +0100

    changes in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import subprocess
 
-with open('requirements.txt', 'r') as file:
-    requirements = file.readlines()
+version = "2.0"
+
+def getRequirements():
+    with open('requirements.txt', 'r') as file:
+        return file.readlines()
+
+    return []
+
+def getVersion():
+
+    subprocess.run(['git','update-index','--refresh'], \
+        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    dirty = subprocess.run(['git','diff-index','--quiet','HEAD']) \
+                .returncode != 0
+    dirtyStr = "-dirty" if dirty else ""
+
+    githash = subprocess.run(['git','rev-parse','--short','HEAD'], \
+            check=True, capture_output=True, text=True) \
+            .stdout.rstrip()
+
+    return f'{version}-{githash}{dirtyStr}'
+
 
 setup(
     name='warp',
-    packages=['warp'],
+    packages=find_packages(),
+    version=getVersion(),
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=getRequirements(),
 )
