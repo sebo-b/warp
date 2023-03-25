45ba4550c6015fc414b960314cdb766e57eef5da
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2023-03-25 21:44:45 +0100

    :pencil: Updated code format with black

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
-import subprocess
 
 version = "2.0"
 
+
 def getRequirements():
-    with open('requirements.txt', 'r') as file:
+    with open("requirements.txt", "r") as file:
         return file.readlines()
 
     return []
 
-#def getVersion():
+
+# def getVersion():
 #
 #    subprocess.run(['git','update-index','--refresh'], \
 #        stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -26,9 +27,9 @@ def getRequirements():
 
 
 setup(
-    name='warp',
+    name="warp",
     packages=find_packages(),
-    version='2.0.dev1',
+    version="2.0.dev1",
     include_package_data=True,
     install_requires=getRequirements(),
 )
